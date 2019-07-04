f91b854b994f69b41f2852e871658ffef1d4f22d
Suraj <suraj@Surajs-MacBook-Air.local>
2019-07-04 18:31:55 +0530

    email and password validation

--- app/services/signing.rb
+++ app/services/signing.rb
@@ -5,9 +5,26 @@ class Signing
     email = params[:email]
     password = params[:password_digest]
     full_name = params[:full_name]
-    
+
+
     if(!(params.has_key?(:email)) || !(params.has_key?(:password_digest)) || !(params.has_key?(:full_name)))
       return {success:false}
+    end
+
+    @len = password.length
+    if(@len < 6 || @len > 12)
+      return {success:"fail", message:"Password length should be between 6 to 12"}
+    end
+
+    @regex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+    if(!email.match(@regex))
+      return {success:"fail", message:"Type a valid email"}
+    end
+
+    user = User.where(email: email)
+    if(user.present?)
+      return {success:"fail", message:"User Already present"}
     end
 
     user = User.create(email:email, password_digest:password, full_name:full_name)
