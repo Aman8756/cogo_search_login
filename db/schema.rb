c276acbe67c1cc80ac9c7eda17f2adc8fb6fef5e
Aman kumar Dokania <dokania_aman@Amans-MacBook-Air.local>
2019-07-04 13:08:57 +0530

    CSRF correction

--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 20190703091105) do
     t.string   "email"
     t.string   "full_name"
     t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
